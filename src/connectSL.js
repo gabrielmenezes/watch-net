d365deaecc4e97496ad652dfa79678663d6cb27a
gsoares <gedex222>
2019-08-13 17:49:41 -0300

    Corrigido modo async

--- src/connectSL.js
+++ src/connectSL.js
@@ -1,33 +1,34 @@
 const https = require('http');
 
-function getDataEm7(uname, pword, path, retorno){
-
-    const options = {
-        host: 'em7.hdbops.net',
-        port: 80,
-        path: path,
-        method: 'GET',
-        headers: {
-            'accept': 'application/json',
-            'Authorization': 'Basic ' + new Buffer.from(uname + ':' + pword).toString('base64')
+ function getDataEm7(uname, pword, path){
+    return new Promise ((resolve, reject) => {
+        const options = {
+            host: 'em7.hdbops.net',
+            port: 80,
+            path: path,
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Basic ' + new Buffer.from(uname + ':' + pword).toString('base64')
+            }
         }
-    }
-
-    https.get(options, (resp) =>{
-        let data = '';
-
-        resp.on('data', (chunk) =>{
-            data+= chunk;
+    
+        https.get(options, (resp) =>{
+            let data = '';
+    
+            resp.on('data', (chunk) =>{
+                data+= chunk;
+            });
+    
+            resp.on('end', () =>{
+                resolve(JSON.parse(data));
         });
-
-        resp.on('end', () =>{
-            console.log(JSON.parse(data));            
-            retorno = JSON.parse(data);
-    });
-    }).on("error",(err)=>{
-        retorno = ("Error: " + err.message);
+            }).on("error",(err)=>{
+                reject("Error: " + err.message);
+            });
     });
 }
-var testRetorno;
-getDataEm7('gsoares','Xdr4593!05','/api/device/', testRetorno);
-console.log(testRetorno);
+
+getDataEm7('gsoares','Xdr4593!05','/api/device/3092')
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
